9a69d047aaf76f84f8f415b494714ed08b57ed1a
ThameeraFernando <sithnuwan111@gmail.com>
2022-10-07 12:24:58 +0530

    test the get by id route

--- test/tasks.js
+++ test/tasks.js
@@ -7,8 +7,8 @@ chai.should();
 
 chai.use(chaiHttp);
 
+// Test the get Route
 describe("Task API", () => {
-  // Test the get Route
   describe("GET /api/tasks", () => {
     it("It should get all the tasks", (done) => {
       chai
@@ -31,9 +31,38 @@ describe("Task API", () => {
         });
     });
   });
-  // Test the get (by id) Route
-  // Test the POST Route
-  // Test the PUT Route
-  // Test the PATCH Route
-  // Test the DELETE Route
 });
+// Test the get (by id) Route
+
+describe("GET /api/tasks/:id", () => {
+  it("It should get a task by id", (done) => {
+    const taskID = 1;
+    chai
+      .request(server)
+      .get("/api/tasks/" + taskID)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("id");
+        res.body.should.have.property("name");
+        res.body.should.have.property("completed");
+        res.body.should.have.property("id").eq(1);
+        done();
+      });
+  });
+  it("It should not get a task by id", (done) => {
+    const taskID = 123;
+    chai
+      .request(server)
+      .get("/api/tasks/" + taskID)
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.text.should.be.eq("The task with the provided ID does not exist.");
+        done();
+      });
+  });
+});
+// Test the POST Route
+// Test the PUT Route
+// Test the PATCH Route
+// Test the DELETE Route
